6b04a0e60f32460157e45b976f0c579ded65a895
Andrew Davison <ajdvsn@gmail.com>
2017-10-30 14:55:00 +0000

    Done 05 tests

--- 05_classes.test.js
+++ 05_classes.test.js
@@ -1,27 +1,29 @@
-/* 
-	
+/*
+
 	Task 1) Refactor the following traditional Javascript "class" into new class syntax
 
 */
 
-function Animal() { }
+// function Animal() { }
 
-Animal.prototype.speak = function() {
-  return "meow";
+// Animal.prototype.speak = function() {
+//   return "meow";
+// }
+
+class Animal {
+  static eat() {
+    return 'nom nom nom';
+  }
 }
-
-Animal.eat = function() {
-  return 'nom nom nom';
-}
-
-const Cat = new Animal();
 
 test('Make cat meow', () => {
+  const Cat = new Animal();
+  Cat.speak = function() {
+    return 'meow';
+  };
 
-    const Cat = new Animal();
-
-    expect(Cat.speak()).toBe('meow');
-    expect(Animal.eat()).toBe('nom nom nom');
+  expect(Cat.speak()).toBe('meow');
+  expect(Animal.eat()).toBe('nom nom nom');
 });
 
 /*
@@ -32,10 +34,14 @@ test('Make cat meow', () => {
 
 */
 
+class Kitten extends Animal {
+  speak() {
+    return 'kitten meow';
+  }
+}
+
 test('Hear the kitten meow', () => {
+  const Kitty = new Kitten();
 
-    const Kitty = new Kitten();
-
-    expect(Kitty.speak()).toBe('kitten meow');
+  expect(Kitty.speak()).toBe('kitten meow');
 });
-
