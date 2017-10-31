c53a516dbb82239f0dfe782c8f363131fa6bc177
Andrew Davison <ajdvsn@gmail.com>
2017-10-31 14:49:29 +0000

    Finished 12_exercise

--- 12_exercise.js
+++ 12_exercise.js
@@ -1,68 +1,46 @@
 /**
  *  AVAILABLE FILTERS:
  *  ["AVAILABLE_IMMEDIATELY", "FRESH_GRAD", "JUNIOR", "JAVASCRIPT", "PHP", "AWS", "REACT", "JAVA"]
- *   
+ *
  *  "AVAILABLE_IMMEDIATELY" and "FRESH_GRAD" will override all the other filters if specified
- * 
+ *
  *  if "AVAILABLE_IMMEDIATELY" and "FRESH_GRAD" are both specified as filter, "FRESH_GRAD" will be ignored
- * 
- * 
+ *
+ *
  *  Exercise: refactor this code
  *  - take care of naming variables
  *  - get rid of the 'for loops'
  *  - move it to modern JS!
  *  - oh, there are missing tests/scenario
- *  
+ *
  *   happy refactory :)
  */
 
-function filter(results, filters) {
-  var out = [];
-  var resultsLength = results.length;
-  var filterLength = filters.length;
-  var hasOptions;
-  var availableImmediately = false;
-  var freshGrad = false;
+function candidateHasFilter(filter, candidate) {
+  return candidate.options.includes(filter);
+}
 
-  if (filterLength !== 0) {
-    if (filters.indexOf('AVAILABLE_IMMEDIATELY') !== -1) {
-      availableImmediately = true;
-    } else if (filters.indexOf('FRESH_GRAD') !== -1) {
-      freshGrad = true;
-    }
+function filter(candidates, filters) {
+  if (filters.length === 0) return candidates;
 
-    for (var i = resultsLength; i--; ) {
-      hasOptions = results[i].options && results[i].options.length > 0; //has.options
+  const availableImmediately = filters.includes('AVAILABLE_IMMEDIATELY');
+  const freshGrad = !availableImmediately && filters.includes('FRESH_GRAD');
 
-      if (results[i].options) {
-        for (var k = filterLength; k--; ) {
-          // loop through filters
-          var hasFilter = false;
-          for (var j = results[i].options.length; j--; ) {
-            if (!availableImmediately && !freshGrad) {
-              if (filters[k].indexOf(results[i].options[j]) !== -1) {
-                hasFilter = true;
-              }
-            } else if (
-              availableImmediately &&
-              results[i].options[j] === 'AVAILABLE_IMMEDIATELY'
-            ) {
-              hasFilter = true;
-            } else if (freshGrad && results[i].options[j] === 'FRESH_GRAD') {
-              hasFilter = true;
-            }
-          }
-          hasOptions = hasOptions && hasFilter;
-        }
+  filterCandidates = candidate => {
+    if (candidate.options && candidate.options.length > 0) {
+      if (
+        availableImmediately &&
+        candidateHasFilter('AVAILABLE_IMMEDIATELY', candidate)
+      ) {
+        return true;
+      } else if (freshGrad && candidateHasFilter('FRESH_GRAD', candidate)) {
+        return true;
       }
-      if (hasOptions) {
-        out.unshift(results[i]);
-      }
+
+      return filters.every(filter => candidateHasFilter(filter, candidate));
     }
-  } else {
-    out = results;
-  }
-  return out;
+  };
+  return candidates.filter(filterCandidates);
 }
 
 module.exports = filter;
